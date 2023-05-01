66fbc14be86f15dcb0f01f532e7aac4ae15824ce
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-05-01 12:22:34 +0200

    SAVEPOINT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,30 @@
-plugins { kotlin("jvm") }
+plugins {
+    java
+    kotlin("jvm") version "1.8.21"
+    `maven-publish`
+}
 
-repositories { mavenCentral() }
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "ktts"
+            artifactId = "ktts-annotations"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
+}
